fca6e186f2c62c1534aacd82ef79644de8e10c1d
vgalko <v.galko@ozitag.com>
2020-03-11 15:53:12 +0300

    update custom smooth scroll

--- main.js
+++ main.js
@@ -7,82 +7,43 @@
 });
 
 const STEP = 10;
-const DURATION = 60;
-const TIMING_FN = t => t * (2 - t);
-
-let trottle = false;
+const DURATION = 1000;
+const TIMING_FN = t => t < .5 ? 2 * t * t : -1 + (4 - 2 * t) * t;
 
 let scrollDirection = null;
-let scrollDistance = null;
 let scrollDuration = null;
 let scrollStartTime = null;
-let scrollStartPos = null;
 let scrollId = null;
 
-let accelerationDistance = STEP;
-let accelerationDuration = DURATION;
-let accelerationStartTime = null;
-let accelerationId = null;
-
 window.addEventListener('wheel', ({ deltaY }) => {
-  if (trottle) return null;
-  trottle = true;
   customSmoothScroll(deltaY > 0 ? 1 : -1);
-  customSmoothAcceleration();
-  setTimeout(() => {
-    trottle = false;
-  }, 150);
 });
 
-function customSmoothScroll (newScrollDirection) {
-  scrollDistance = STEP;
+function customSmoothScroll(newScrollDirection) {
   scrollDuration = DURATION;
-  scrollDirection = newScrollDirection;
-
-  scrollStartPos = pageYOffset;
   scrollStartTime = performance.now();
-  cancelAnimationFrame(scrollId);
-  scrollId = requestAnimationFrame(animateScroll);
+
+  if (scrollDirection !== newScrollDirection) {
+    cancelAnimationFrame(scrollId);
+    scrollDirection = newScrollDirection;
+    scrollId = requestAnimationFrame(animateScroll);
+  }
 }
 
-function animateScroll (currTime) {
+function animateScroll(currTime) {
   let timeFraction = (currTime - scrollStartTime) / scrollDuration;
   if (timeFraction > 1) timeFraction = 1;
 
-  let progress = scrollStartPos + TIMING_FN(timeFraction) * scrollDistance * scrollDirection;
-  progress = progress.toFixed(2);
-  scrollTo(0, progress);
+  let progress = ((STEP - (STEP * TIMING_FN(timeFraction))) * scrollDirection).toFixed(0);
+  scrollTo(0, pageYOffset + parseInt(progress));
 
   if (timeFraction < 1) {
     scrollId = requestAnimationFrame(animateScroll);
   } else {
-    scrollDistance = null;
+    cancelAnimationFrame(scrollId);
     scrollDuration = null;
-    scrollStartTime = null;
     scrollDirection = null;
-    scrollStartPos = pageYOffset;
-  }
-}
-
-function customSmoothAcceleration () {
-  accelerationStartTime = performance.now();
-  cancelAnimationFrame(accelerationId);
-  accelerationId = requestAnimationFrame(animateAcceleration);
-}
-
-function animateAcceleration (currTime) {
-  let timeFraction = (currTime - accelerationStartTime) / accelerationDuration;
-  if (timeFraction > 1) timeFraction = 1;
-
-  let progress = TIMING_FN(timeFraction) * accelerationDistance;
-  progress = progress.toFixed(2);
-  scrollDuration += DURATION * progress;
-  scrollDistance += STEP * progress;
-
-  if (timeFraction < 1) {
-    accelerationId = requestAnimationFrame(animateAcceleration);
-  } else {
-    accelerationStartTime = null;
-    accelerationId = null;
+    scrollStartTime = null;
+    scrollId = null;
   }
 }
