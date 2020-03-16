82356c2faec62b9d43c031a334da61fe339aff66
PainIncoming <galko3.v@gmail.com>
2020-03-16 04:44:23 +0300

    make smooth scroll acceleration

--- main.js
+++ main.js
@@ -6,8 +6,9 @@
   });
 });
 
-const STEP = 10;
+const STEP = 5;
 const DURATION = 1000;
+const ACCELERATION = .9;
 const TIMING_FN = t => t < .5 ? 2 * t * t : -1 + (4 - 2 * t) * t;
 
 let scrollDirection = null;
@@ -15,35 +16,55 @@ let scrollDuration = null;
 let scrollStartTime = null;
 let scrollId = null;
 
+let acceleration = ACCELERATION;
+let accelerationId = null;
+
 window.addEventListener('wheel', ({ deltaY }) => {
   customSmoothScroll(deltaY > 0 ? 1 : -1);
+  accelerateScroll();
 });
 
-function customSmoothScroll(newScrollDirection) {
+function customSmoothScroll (newScrollDirection) {
   scrollDuration = DURATION;
   scrollStartTime = performance.now();
 
   if (scrollDirection !== newScrollDirection) {
+    acceleration = ACCELERATION;
     cancelAnimationFrame(scrollId);
     scrollDirection = newScrollDirection;
     scrollId = requestAnimationFrame(animateScroll);
   }
 }
 
-function animateScroll(currTime) {
+function animateScroll (currTime) {
   let timeFraction = (currTime - scrollStartTime) / scrollDuration;
   if (timeFraction > 1) timeFraction = 1;
 
-  let progress = ((STEP - (STEP * TIMING_FN(timeFraction))) * scrollDirection).toFixed(0);
+  let progress = (STEP - (STEP * TIMING_FN(timeFraction))) * scrollDirection;
+  progress = (progress * acceleration).toFixed(0);
   scrollTo(0, pageYOffset + parseInt(progress));
 
   if (timeFraction < 1) {
     scrollId = requestAnimationFrame(animateScroll);
   } else {
+    acceleration = ACCELERATION;
     cancelAnimationFrame(scrollId);
     scrollDuration = null;
     scrollDirection = null;
     scrollStartTime = null;
     scrollId = null;
   }
+}
+
+function accelerateScroll () {
+  acceleration += .1;
+  if (accelerationId) return null;
+  accelerationId = setInterval(() => {
+    acceleration -= .1;
+    if (acceleration < ACCELERATION) {
+      acceleration = ACCELERATION;
+      clearInterval(accelerationId);
+      accelerationId = null;
+    }
+  }, 50);
 }
